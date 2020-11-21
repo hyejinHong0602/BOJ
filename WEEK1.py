6ef17e7547c313980d9baedeece537ba70dae2d3
hhz2000 <hhz2000@naver.com>
2020-11-22 03:44:05 +0900

    2438, 2739, 2741, 2742, 2439

--- WEEK1.py
+++ WEEK1.py
@@ -34,3 +34,36 @@
 # print(max_num)
 # print(max_order)
 #이렇게 한거는 런타임 에러 (최대값 100을 제한하지 못함)
+
+#2438번
+# n=int(input('n='))
+#
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print('*',end='')
+#     print()
+
+#2739번
+# n=int(input())
+# for i in range(1,10):
+#     print(f'{n} * {i} = {n*i}')
+
+#2741번
+# n=int(input())
+# for i in range(1,n+1):
+#     print(i)
+
+#2742번
+# n=int(input())
+# for i in range(1,n+1):
+#     print(n+1-i)
+
+#2439번
+# n=int(input())
+# for i in range(1,n+1):
+#     for j in range(n-i):
+#         print(' ',end='')
+#     for k in range(i):
+#         print('*',end='')
+#     print()
+
