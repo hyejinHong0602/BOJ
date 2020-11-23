b50b56138bd059a53db44f776610fbf1d8baef5a
hhz2000 <hhz2000@naver.com>
2020-11-23 21:58:13 +0900

    2577, 3052

--- WEEK1.py
+++ WEEK1.py
@@ -189,3 +189,57 @@
 #         print(input())
 #     except EOFError:
 #         break
+
+#2577
+# a=int(input())
+# b=int(input())
+# c=int(input())
+#
+# res=a*b*c
+# n=1#자릿수
+# numbers=[0,0,0,0,0,0,0,0,0,0]
+# while res>10:
+#     res=res//10
+#     n=n+1
+#
+# res=a*b*c
+# for i in range(n):
+#     t=res % 10
+#     res = res // 10
+#     for j in range(10):
+#         if t==j:
+#             numbers[j]=numbers[j]+1
+#
+# for k in range(10):
+#     print(numbers[k])
+
+#3052
+# numbers=[] #숫자 10개 배열로 저장
+# index=[]
+# check=0
+# for i in range(10):
+#     numbers.append(int(input()))
+#
+# for j in range(10):
+#     t=numbers[j]%42
+#
+#     if len(index)==0:
+#         index.append(t)
+#     else:
+#         for k in range(len(index)):
+#             if index[k] == t:
+#                 check=1
+#             else:
+#                 continue
+#         if check==0:
+#             index.append(t)
+#
+# print(len(index))
+# 실행결과가 나오는데 답은 틀렸다고 해서 코드를 찾아보니 아래 코드가 나왔다.
+# set 함수를 이용하면 간단하게 중복은 제거하고 arr에 넣을 수 있다.
+# arr = []
+# for i in range(10):
+#     n = int(input())
+#     arr.append(n % 42)
+# arr = set(arr)
+# print(len(arr))
