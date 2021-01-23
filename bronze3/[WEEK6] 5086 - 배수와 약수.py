2952c1ee95cddb4fce85d916e965a59502207da3
hhz2000 <hhz2000@naver.com>
2021-01-23 21:43:02 +0900

    [WEEK7] 1427

---
+++ bronze3/[WEEK6] 5086 - 배수와 약수.py
@@ -0,0 +1,14 @@
+a=1
+b=1
+
+while a!=0 or b!=0:
+    a,b = map(int,input().split())
+    if a==0 and b==0:
+        break
+    else:
+        if a % b == 0:
+            print('multiple')
+        elif b % a == 0:
+            print('factor')
+        else:
+            print('neither')
---
+++ silver5/[WEEK7] 1427 - 소트인사이드.py
@@ -0,0 +1,8 @@
+
+n =input()
+n_list = list(n)
+idx=len(n_list)
+
+n_sort=sorted(n_list)
+for i in range(idx-1, -1, -1):
+    print(n_sort[i],end='')
