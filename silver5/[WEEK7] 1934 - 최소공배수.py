33ed3b1a65ca0b0333410b03446d54bb9b69e823
hhz2000 <hhz2000@naver.com>
2021-01-27 02:10:22 +0900

    [WEEK7] 1934

---
+++ silver5/[WEEK7] 1934 - 최소공배수.py
@@ -0,0 +1,19 @@
+n = int(input())
+for i in range(n):
+    a, b = map(int, input().split())
+
+    if a > b:
+        a, b = b, a
+    multiple_ab = a * b
+
+    mod = b % a
+    while mod != 0:
+        if mod != 0:
+            b = a
+            a = mod
+            mod = b % a
+
+
+    gcd = a
+    print(multiple_ab // gcd)
+     # 최소공배수
