d49586e83c9d82321a9a1b9b29e1ad97c85994b2
hhz2000 <hhz2000@naver.com>
2021-02-14 00:30:28 +0900

    [WEEK10] 11653 수정

--- silver/[WEEK10] 11653 - 소인수분해.py
+++ silver/[WEEK10] 11653 - 소인수분해.py
@@ -1,12 +1,14 @@
-n = int(input())
+import sys
+n = int(sys.stdin.readline())
 prime_list = []
 
-while True:
-    for i in range(2, n):
-        if n % i == 0:
-            n = n // i
-            prime_list.append(i)
+while n!=1:
+    for j in range(2, n+1): # 이거 n+1이 아니라 n으로 해서 헤맴.
+        if n % j == 0:
+            n = n // j
+            prime_list.append(j)
             break
 
-
+for i in prime_list:
+    print(i)
 
